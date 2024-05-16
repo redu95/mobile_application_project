45f5378588133f80b80a2178287f201f2c26de1b
Fenet-damena <fenetdamena74@gmail.com>
2024-05-16 18:47:31 +0300

    menu added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,7 @@
-//home_page.dart
-
-
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_application_project/setting_page.dart';
-
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,8 +9,6 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
-
-
 
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
@@ -34,7 +29,6 @@ class _HomeState extends State<Home> {
       _selectedIndex = index;
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -66,33 +60,171 @@ class _HomeState extends State<Home> {
 }
 
 class HomePage extends StatelessWidget {
-   HomePage({super.key});
+  HomePage({super.key});
 
   final user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome,' + user!.email!,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-
-          ],
+    return SafeArea(
+      child: Scaffold(
+          body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Icon(Icons.menu_rounded, size: 40),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(22),
+                child: const Image(
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/icon_imgg/user_im.jpg'),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-=======
-      backgroundColor: Colors.deepPurpleAccent,
+        const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+            child: Text("Where Would You Like To Go?",
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 30,
+                    color: Color(0xff3c4657)))),
+        SizedBox(
+          height: 20,
 
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 320,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search for place",
+                    prefixIcon:
+                        Icon(Icons.search, size: 35, color: Color(0xff3c4657)),
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2.4, color: Color(0xff3c4657)),
+                        borderRadius: BorderRadius.circular(12)),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                        const BorderSide(width: 2.4,color: Color(0xff3c4657))
+                    )
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xff3c4657),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 50,
+                width: 50,
+                child:const  Icon(Icons.filter_alt, size: 35, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+            Container(
+              height: 70,
+              width: double.infinity,
+
+              child:ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                Container(
+                    height:60,
+                    width: 100,
+                  margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+
+                  decoration: BoxDecoration(
+                    color:Color(0xffF0F1F3),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                    child:const Center(
+                        child:Text("Hotel", style:TextStyle(fontSize: 26),)
+                )
+                ),
+                  Container(
+                      height:60,
+                      width: 120,
+                      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+
+                      decoration: BoxDecoration(
+                        color:Color(0xffF0F1F3),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    child:const Center(
+                  child:Text("Apartment", style:TextStyle(fontSize: 22),)
+            )),
+                  Container(
+                      height:60,
+                      width: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+
+                      decoration: BoxDecoration(
+                        color:Color(0xffF0F1F3),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child:const Center(
+                          child:Text("Motel", style:TextStyle(fontSize: 22),)
+                      )
+                  ),
+                  Container(
+                      height:60,
+                      width: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+
+                      decoration: BoxDecoration(
+                        color:Color(0xffF0F1F3),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child:const Center(
+                          child:Text("Hotel", style:TextStyle(fontSize: 26),)
+                      )
+                  ),
+                  Container(
+                      height:60,
+                      width: 120,
+                      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+
+                      decoration: BoxDecoration(
+                        color:Color(0xffF0F1F3),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child:const Center(
+                          child:Text("Apartment", style:TextStyle(fontSize: 22),)
+                      )),
+                  Container(
+                      height:60,
+                      width: 100,
+                      margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12),
+
+                      decoration: BoxDecoration(
+                        color:Color(0xffF0F1F3),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child:const Center(
+                          child:Text("Motel", style:TextStyle(fontSize: 22),)
+                      )
+                  ),
+
+                ]
+                )
+
+              ,
+            )
+      ]
+          )
+      ),
     );
   }
 }
-
-
-
