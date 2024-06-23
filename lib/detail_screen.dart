5033224dbf7b35ad5eeb374cb8897ef46b838b43
72730882 <faizamohammed72730882@gmail.com>
2024-06-23 01:55:18 -1200

    applying Color arrangment setting thought the app

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobile_application_project/colors.dart';
+
 import 'booking.dart';
 
 class Detail extends StatelessWidget {
