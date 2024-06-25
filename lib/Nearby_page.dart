850e8dbbd703eed097a93d96f097e399b7f64c8e
tsehay <ttsehay499@gmail.com>
2024-06-25 12:47:03 +0300

    searchflag

--- lib/Nearby_page.dart
+++ lib/Nearby_page.dart
@@ -291,7 +291,64 @@ class _NearbyPageState extends ConsumerState<NearbyPage> {
                           )
                         ]),
                       )
-                    : Container(),    
+                    : Container(), 
+                searchFlag.searchToggle
+                    ? allSearchResults.allReturnedResults.length != 0
+                        ? Positioned(
+                            top: 100.0,
+                            left: 15.0,
+                            child: Container(
+                              height: 200.0,
+                              width: screenWidth - 30.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Colors.white.withOpacity(0.7),
+                              ),
+                              child: ListView(
+                                children: [
+                                  ...allSearchResults.allReturnedResults
+                                      .map((e) => buildListItem(e, searchFlag))
+                                ],
+                              ),
+                            ))
+                        : Positioned(
+                            top: 100.0,
+                            left: 15.0,
+                            child: Container(
+                              height: 200.0,
+                              width: screenWidth - 30.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Colors.white.withOpacity(0.7),
+                              ),
+                              child: Center(
+                                child: Column(children: [
+                                  Text('No results to show',
+                                      style: TextStyle(
+                                          fontFamily: 'WorkSans',
+                                          fontWeight: FontWeight.w400)),
+                                  SizedBox(height: 5.0),
+                                  Container(
+                                    width: 125.0,
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        searchFlag.toggleSearch();
+                                      },
+                                      child: Center(
+                                        child: Text(
+                                          'Close this',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'WorkSans',
+                                              fontWeight: FontWeight.w300),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ]),
+                              ),
+                            ))
+                    : Container(),   
       ])
     ])));
   }
