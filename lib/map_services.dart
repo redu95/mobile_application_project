26c88aba6b2965474744f736366c8fc3e67e8128
tsehay <ttsehay499@gmail.com>
2024-06-25 13:11:42 +0300

    map services

--- lib/map_services.dart
+++ lib/map_services.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:googlemapsapp/models/auto_complete_result.dart';
+import 'package:mobile_application_project/auto_complete_result.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
